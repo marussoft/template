47e30573ee7d89dfc1e402179ded9aec94206b21
milinsky <mail@milinsky.ru>
2019-09-25 21:41:25 +1000

    fix render method

--- src/Template.php
+++ src/Template.php
@@ -10,19 +10,25 @@ class Template
     
     private $variables = [];
 
-    public function __construct(string $pathToViews)
+    public function __construct()
     {
-        $loader = new \Twig\Loader\FilesystemLoader($pathToViews);
-        $this->twig = new \Twig\Environment($loader);
+        $this->loader = new \Twig\Loader\FilesystemLoader(Config::get('kernel.template', 'path_to_view'));
+        $this->twig = new \Twig\Environment($this->loader);
     }
 
-    public function content(array $variables)
+    public function content(array $variables) : self
     {
         $this->variables = array_merge($this->variables, $variables);
+        return $this;
     }
     
-    public function render(string $template)
+    public function render(string $template) : string
     {
         return $this->twig->render($template . '.twig', $this->variables);
+    }
+    
+    public function exists(string $template) : bool
+    {
+        return $this->loader->exists($template . '.twig');
     }
 }
